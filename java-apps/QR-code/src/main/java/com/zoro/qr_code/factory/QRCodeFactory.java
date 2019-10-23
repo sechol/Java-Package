7f3318ff91319452fad20b4514a0e3b5b48fe2a4
zoro-lonly <329465634@qq.com>
2019-10-23 18:28:58 +0800

    qrCode commit

---
+++ java-apps/QR-code/src/main/java/com/zoro/qr_code/factory/QRCodeFactory.java
@@ -0,0 +1,39 @@
+﻿package com.zoro.qr_code.factory;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * @program: Java-Package
+ * @description: Factory class that generates QR code!
+ * @author: Zoro Li
+ * @create: 2019-10-23 18:00
+ */
+public class QRCodeFactory {
+
+    private static final int WIDTH = 350;
+    private static final int HEIGHT = 350;
+    private static final String QR_CODE_STORAGE_PATH = "/qr-code";
+    private static QRCodeWriter factory = new QRCodeWriter();
+
+    public static void generateQRCodeImage(String content) {
+
+        try {
+            BitMatrix bitMatrix = factory.encode(content, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
+            Path path = FileSystems.getDefault().getPath(QR_CODE_STORAGE_PATH);
+            MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
---
+++ java-apps/QR-code/src/test/java/QRCodeTest.java
@@ -0,0 +1,11 @@
+﻿/**
+ * @program: Java-Package
+ * @description: test
+ * @author: Zoro Li
+ * @create: 2019-10-23 18:24
+ */
+public class QRCodeTest {
+
+
+
+}
